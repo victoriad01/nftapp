4d6d93678aa7ca5cfb17dd2721cc88301586d6b2
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-03-30 22:34:36 +0100

    first commit

---
+++ components/Button.js
@@ -0,0 +1,55 @@
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native'
+import { COLORS, SIZES, FONTS, SHADOWS } from '../constants'
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        borderRadius: SIZES.extraLarge,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode='contain'
+        style={{ height: 24, width: 24 }}
+      />
+    </TouchableOpacity>
+  )
+}
+
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+
+        borderRadius: SIZES.extraLarge,
+        midWidth: minWidth,
+        padding: SIZES.small,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: 'center',
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ components/index.js
@@ -0,0 +1,17 @@
+import FocusedStatusBar from './FocusedStatusBar'
+import HomeHeader from './HomeHeader'
+import NFTCard from './NFTCard'
+import { CircleButton, RectButton } from './Button'
+
+import DetailsBid from './DetailsBid'
+import DetailsDesc from './DetailsDesc'
+
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+}
